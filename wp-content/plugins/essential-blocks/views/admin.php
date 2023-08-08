9f1025a8996d95947ebb9d71359e38ede28d08a7
Chathura Gunasekara <chathuragunasekera@gmail.com>
2023-08-09 03:04:55 +0530

    static export

--- wp-content/plugins/essential-blocks/views/admin.php
+++ wp-content/plugins/essential-blocks/views/admin.php
@@ -1,9 +1,1 @@
-<?php
-    // Exit if accessed directly.
-    if ( ! defined( 'ABSPATH' ) ) {
-        exit;
-    }
-?>
-<div class="essential-blocks-admin-page-wrapper eb-settings-wrap">
-    <div id="eb-settings"></div>
-</div>
+ 
